a7fcc7a13fd17bc6cb948aaf5a19d5c021bc046e
Horizon-Research-UG <162907990+Horizon-Research-UG@users.noreply.github.com>
2025-09-01 16:01:42 +0200

    V1

---
+++ Problem Set 0/Indoor_Voice/indoor_voice.py
@@ -0,0 +1,16 @@
+# Name: indoor.py 
+# Meta: implement a program in Python
+# Ticket 1: that prompts the user for input and then 
+print()
+x = input("Hallo, schreib mal bitte was : )  ---->  : ")
+print()
+
+# outputs that same input in lowercase.
+print(x.lower())
+print()
+#   Punctuation and whitespace should be outputted unchanged. 
+# 
+# You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+
+
+# so Aufgabe erfüllt -> schonmal pushen
